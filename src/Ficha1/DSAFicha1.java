8b1056b16415be7e66ac475307fd123160a3fb5d
Diogo <Diogo@DESKTOP-94ETDT4>
2025-09-26 09:47:15 +0100

    Ficha1

---
+++ src/Ficha1/DSAFicha1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ficha1;
+
+/**
+ *
+ * @author Diogo
+ */
+public class DSAFicha1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        
+        Matematica teste = new Matematica();
+        
+        System.out.println(teste.factorialI(5));
+        System.out.println(teste.factorialR(5));
+        
+        
+        
+    }
+    
+}
---
+++ src/Ficha1/Matematica.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ficha1;
+
+/**
+ *
+ * @author Diogo
+ */
+public class Matematica {
+    
+    
+    
+    // 5N + 4   => O(N) Complexidade temporal Linear
+    
+    
+    // num , f, i, return  (4 variaveis) => S(1) Complexidade espacial Constante
+
+    public static int factorialI(int num) {
+
+        int f = 1;          // 1
+
+        for (int i = 1; i <= num; i++) { // 1 + (n+1) + 2N
+
+            f *= i; // 2N
+
+        }
+
+        return f;   // 1
+
+    }
+    
+    
+    // 5N + 2
+    // O(N) Complexidade temporal linear
+    // num + return * (N+1 da recursividade) = 2N + 2
+    // S(N) Complexidade espacial linear 
+    
+
+    public static int factorialR(int num) {
+        
+        
+        if (num == 0){        // 1 * (N+1) = N + 1
+            return 1;          // 1
+        }else{
+            
+           
+           return num * factorialR(num-1);        // (1 + 1 + 1 + 1) * N = 4N
+        }
+        
+
+    }
+
+}
